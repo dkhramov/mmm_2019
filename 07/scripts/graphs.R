f4b885cb32cc7a1887d529f299a721067eaa6dbc
Dmitry A. Khramov <dkhramov@mail.ru>
2019-05-02 18:23:53 +0300

    Исправленная лекция 7

--- 07/scripts/graphs.R
+++ 07/scripts/graphs.R
@@ -183,3 +183,140 @@ DiagrammeR::grViz("
     6 -> 7[label='I=2', color='red'];
   }
 ")
+
+##########
+
+DiagrammeR::grViz("
+  digraph mydigraph {
+                  node[shape=circle];
+                  rankdir='LR';
+                  1 -> 2 [label=A];
+                  2  [style = invis];
+                  1 -> 3 [label=B];
+                  3  [style = invis]; 
+                  2 -> 3 [style = invis];
+                  4  [style = invis];
+                  2 -> 4 [style = invis];
+                  5  [style = invis];
+                  2 -> 5 [style = invis];
+                  4 -> 5 [style = invis];
+                  6  [style = invis];
+                  3 -> 6 [style = invis];
+                  5 -> 6 [style = invis];
+                  7  [style = invis];
+                  6 -> 7 [style = invis];
+                  }
+                  ")
+
+DiagrammeR::grViz("
+                  digraph mydigraph {
+                  node[shape=circle];
+                  rankdir='LR';
+                  1 -> 2 [label=A];
+                  2
+                  1 -> 3 [label=B];
+                  3  [style = invis]; 
+                  2 -> 3 [label=C];
+                  4  [style = invis];
+                  2 -> 4 [label=E];
+                  5  [style = invis];
+                  2 -> 5 [label=D];
+                  4 -> 5 [style = invis];
+                  6  [style = invis];
+                  3 -> 6 [style = invis];
+                  5 -> 6 [style = invis];
+                  7  [style = invis];
+                  6 -> 7 [style = invis];
+                  }
+                  ")
+
+DiagrammeR::grViz("
+                  digraph mydigraph {
+                  node[shape=circle];
+                  rankdir='LR';
+                  1 -> 2 [label=A];
+                  2
+                  1 -> 3 [label=B];
+                  3
+                  2 -> 3 [label=C];
+                  4
+                  2 -> 4 [label=E];
+                  5  [style = invis];
+                  2 -> 5 [label=D];
+                  4 -> 5 [label=F];
+                  6  [style = invis];
+                  3 -> 6 [label=H];
+                  5 -> 6 [style = invis];
+                  7  [style = invis];
+                  6 -> 7 [style = invis];
+                  }
+                  ")
+
+DiagrammeR::grViz("
+                  digraph mydigraph {
+                  node[shape=circle];
+                  rankdir='LR';
+                  1;
+                  1 -> 2 [label=A];
+                  2;
+                  1 -> 3 [label=B];
+                  3; 
+                  2 -> 3 [label=C];
+                  4;
+                  2 -> 4 [label=E];
+                  5;
+                  2 -> 5  [label=D];
+                  4 -> 5 [label=F];
+                  6;
+                  3 -> 6 [label=H];
+                  5 -> 6 [label=G];
+                  7 [style = invis];
+                  6 -> 7[label=I];
+                  }
+                  ")
+
+DiagrammeR::grViz("
+                  digraph mydigraph {
+                  node[shape=circle];
+                  rankdir='LR';
+                  1;
+                  1 -> 2 [label=A];
+                  2;
+                  1 -> 3 [label=B];
+                  3; 
+                  2 -> 3 [label=C];
+                  4;
+                  2 -> 4 [label=E];
+                  5;
+                  2 -> 5  [label=D];
+                  4 -> 5 [label=F];
+                  6 [style = invis];
+                  3 -> 6 [label=H];
+                  5 -> 6 [label=G];
+                  7 [style = invis];
+                  6 -> 7[style = invis];
+                  }
+                  ")
+
+DiagrammeR::grViz("
+                  digraph mydigraph {
+                  node[shape=circle];
+                  rankdir='LR';
+                  1 -> 2 [label=A];
+                  2;
+                  1 -> 2 [label=B];
+                  }
+                  ")
+
+DiagrammeR::grViz("
+                  digraph mydigraph {
+                  node[shape=circle];
+                  rankdir='LR';
+                  1 -> 2 [label=A];
+                  2;
+                  1 -> 3 [label=B];
+                  3; 
+                  3 -> 2 [style = dashed, label='C=0'];
+                  }
+                  ")
+
